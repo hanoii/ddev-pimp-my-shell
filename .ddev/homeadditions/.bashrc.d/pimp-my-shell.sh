fbdac239f6cbfc15c4a05d3ec19d0dc61df947c2
Ariel Barreiro <abarrei@gmail.com>
2024-04-05 09:22:57 -0300

    pimp-my-shell.sh only on interactive
    This also fixes an issue between the trap and fzf-git.
    Also simplified sourcing fzf and fzf-git as they are bound the be there

--- .ddev/homeadditions/.bashrc.d/pimp-my-shell.sh
+++ .ddev/homeadditions/.bashrc.d/pimp-my-shell.sh
@@ -1,5 +1,11 @@
 #ddev-generated
 
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
 # This is so that child processes have appropriate access to this var
 export SHELL
 
@@ -10,12 +16,10 @@ export EDITOR=${DDEV_PIMP_MY_SHELL_EDITOR-/usr/bin/vim}
 COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
 
 # fzf
-[ -f /opt/.fzf.bash ] && source /opt/.fzf.bash
+source /opt/.fzf.bash
 
 # fzf-git
-if ! grep -qxF "source /opt/fzf-git.sh/fzf-git.sh" ~/.bashrc ; then
-  echo -e "\n# Added by ddev-fzf add-on on $(date -u "+%Y-%m-%d %H:%m") \nsource /opt/fzf-git.sh/fzf-git.sh" >> ~/.bashrc
-fi
+source /opt/fzf-git.sh/fzf-git.sh
 
 # z.lua
 mkdir -p /mnt/ddev-global-cache/z.lua/${HOSTNAME}

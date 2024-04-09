ba3506251c3b96b384a34ebf820ac4ea6c6ca58f
Ariel Barreiro <abarrei@gmail.com>
2024-04-09 17:47:19 -0300

    z.lua proper path on fish

--- .ddev/homeadditions/.config/fish/conf.d/pimp-my-shell.fish
+++ .ddev/homeadditions/.config/fish/conf.d/pimp-my-shell.fish
@@ -1,4 +1,10 @@
 #ddev-generated
+
+# z.lua
+mkdir -p /mnt/ddev-global-cache/z.lua/$HOSTNAME
+set -x _ZL_DATA /mnt/ddev-global-cache/z.lua/$HOSTNAME/.zlua
+lua /opt/z.lua/z.lua --init fish | source
+
 function fish_title
   echo "$DDEV_PROJECT ddev@"(fish_prompt_pwd_dir_length=1 prompt_pwd) - $argv
 end
--- .ddev/homeadditions/.config/fish/conf.d/z.fish
+++
@@ -1,3 +0,0 @@
-#ddev-generated
-
-lua /opt/z.lua/z.lua --init fish | source

f6bd8b0aeb8073eafd3a28f10734d6b0c7ab3b73
Ariel Barreiro <abarrei@gmail.com>
2024-04-12 12:12:52 -0300

    Change the way fish history is kept across restart

--- homeadditions/.config/fish/conf.d/pimp-my-shell.fish
+++ homeadditions/.config/fish/conf.d/pimp-my-shell.fish
@@ -8,9 +8,15 @@ lua /opt/z.lua/z.lua --init fish | source
 # fish
 mkdir -p /mnt/ddev-global-cache/fishhistory/$HOSTNAME
 # workaround to keep the history file accross restarts
-[ -f ~/.local/share/fish/fish_history ] && [ ! -L ~/.local/share/fish/fish_history ] && mv ~/.local/share/fish/fish_history /mnt/ddev-global-cache/fishhistory/$HOSTNAME/fish_history
-[ ! -f ~/.local/share/fish/fish_history ] && touch /mnt/ddev-global-cache/fishhistory/$HOSTNAME/fish_history
-ln -fs /mnt/ddev-global-cache/fishhistory/$HOSTNAME/fish_history ~/.local/share/fish/fish_history
+# https://fishshell.com/docs/current/cmds/history.html#customizing-the-name-of-the-history-file
+# I am not using $XDG_DATA_HOME as that would change a lot of things, I rather
+# just keep things default and do this cp workaround.
+function handler --on-event fish_postexec
+    cp ~/.local/share/fish/fish_history /mnt/ddev-global-cache/fishhistory/$HOSTNAME/fish_history
+end
+if test -f /mnt/ddev-global-cache/fishhistory/$HOSTNAME/fish_history
+  cp /mnt/ddev-global-cache/fishhistory/$HOSTNAME/fish_history ~/.local/share/fish/fish_history
+end
 
 function fish_title
   echo "$DDEV_PROJECT ddev@"(fish_prompt_pwd_dir_length=1 prompt_pwd) - $argv

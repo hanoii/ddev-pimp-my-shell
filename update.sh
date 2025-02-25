c475ce4018a4256ec445e07727f677c722d9ea84
Ariel Barreiro <abarrei@gmail.com>
2025-02-25 15:58:58 -0300

    feat: update.sh rust and some cleanup

--- update.sh
+++ update.sh
@@ -7,12 +7,12 @@ VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/ahoy-cli
 echo "ahoy version: $VERSION"
 perl -pi -e "s@AHOY_VERSION=.*@AHOY_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
-# fzfv
+# fzf*
 VERSION=$(curl -s "https://github.com/junegunn/fzf/commits" | grep -o 'commit/[a-f0-9]\{40\}' | head -1 | cut -d'/' -f2)
 echo "fzf latest commit SHA1: $VERSION"
 perl -pi -e "s@FZF_SHA1=.*@FZF_SHA1=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 VERSION=$(curl -s "https://github.com/junegunn/fzf-git.sh/commits" | grep -o 'commit/[a-f0-9]\{40\}' | head -1 | cut -d'/' -f2)
-echo "fzf-git.sh latest commit SHA1: $VERSION"
+echo "fzf-git.sh (bash) latest commit SHA1: $VERSION"
 perl -pi -e "s@FZFGIT_SHA1=.*@FZFGIT_SHA1=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
 # bat
@@ -25,7 +25,7 @@ VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/skywind3
 echo "z.lua version: $VERSION"
 perl -pi -e "s@ZLUA_VERSION=.*@ZLUA_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
-# starship
+# starship (bash)
 VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/starship/starship/releases/latest" | sed 's/.*tag\/v//g')
 echo "starship version: $VERSION"
 perl -pi -e "s@STARSHIP_VERSION=.*@STARSHIP_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
@@ -52,17 +52,17 @@ perl -pi -e "s@GO_VERSION=.*@GO_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile
 
 # fisher
 VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/jorgebucaran/fisher/releases/latest" | sed 's/.*tag\///g')
-echo "fisher version: $VERSION"
+echo "fisher (fish) version: $VERSION"
 perl -pi -e "s@FISHER_VERSION=.*@FISHER_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
 # tide
 VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/IlanCosman/tide/releases/latest" | sed 's/.*tag\/v//g')
-echo "tide version: $VERSION"
+echo "tide (fish) version: $VERSION"
 perl -pi -e "s@TIDE_VERSION=.*@TIDE_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
 # bass
 VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/edc/bass/releases/latest" | sed 's/.*tag\/v//g')
-echo "bass version: $VERSION"
+echo "bass (fish) version: $VERSION"
 perl -pi -e "s@BASS_VERSION=.*@BASS_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
 # bun
@@ -70,36 +70,48 @@ VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/oven-sh/
 echo "bun version: $VERSION"
 perl -pi -e "s@BUN_VERSION=.*@BUN_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
+# sysbox
 VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/skx/sysbox/releases/latest" | sed 's/.*tag\/release-//g')
-echo "sysbox version: $VERSION"
+echo "sysbox (go) version: $VERSION"
 perl -pi -e "s@SYSBOX_VERSION=.*@SYSBOX_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
+# rust
+VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/rust-lang/rust/releases/latest" | sed 's/.*tag\///g')
+echo "rust version: $VERSION"
+perl -pi -e "s@RUST_VERSION=.*@RUST_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
+
+#spacer
 VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/samwho/spacer/releases/latest" | sed 's/.*tag\/v//g')
-echo "spacer version: $VERSION"
+echo "spacer (rust) version: $VERSION"
 perl -pi -e "s@SPACER_VERSION=.*@SPACER_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
+# delta
 VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/dandavison/delta/releases/latest" | sed 's/.*tag\///g')
-echo "git-delta version: $VERSION"
+echo "git-delta (rust) version: $VERSION"
 perl -pi -e "s@DELTA_VERSION=.*@DELTA_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
+# kitty
 VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/kovidgoyal/kitty/releases/latest" | sed 's/.*tag\/v//g')
 echo "kitty version: $VERSION"
 perl -pi -e "s@KITTY_VERSION=.*@KITTY_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
+# terminaltexteffects
 VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/ChrisBuilds/terminaltexteffects/releases/latest" | sed 's/.*tag\/release-//g')
 echo "terminaltexteffects version: $VERSION"
 perl -pi -e "s@TTE_VERSION=.*@TTE_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
+# lazygit
 VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/jesseduffield/lazygit/releases/latest" | sed 's/.*tag\/v//g')
 echo "lazygit version: $VERSION"
 perl -pi -e "s@LAZYGIT_VERSION=.*@LAZYGIT_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
+# ov
 VERSION=$(curl -Ls -o /dev/null -w %{url_effective} "https://github.com/noborus/ov/releases/latest" | sed 's/.*tag\/v//g')
 echo "ov - feature rich terminal pager version: $VERSION"
 perl -pi -e "s@OV_VERSION=.*@OV_VERSION=${VERSION}; \\\\@g" web-build/Dockerfile.pimp-my-shell
 
 if git diff --exit-code web-build/Dockerfile.pimp-my-shell; then
-  echo -e "\nThere are currently no upgrades."
+  echo -e "\nThere are currently no available upgrades."
 else
   ddev add-on get . > /dev/null 2>&1
 fi

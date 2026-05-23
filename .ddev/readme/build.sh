5733497413c71d25731d25bf806b728d5f3854ed
Ariel Barreiro <abarrei@gmail.com>
2026-05-22 21:00:58 -0300

    build(ddev): hanoii/ddev-readme update

--- .ddev/readme/build.sh
+++ .ddev/readme/build.sh
@@ -2,5 +2,5 @@
 #ddev-generated
 set -e -o pipefail
 
-prettier /var/www/html/README.md --write --config=/var/www/html/.ddev/readme/.prettierrc
-markdown-toc -i /var/www/html/README.md
+$(/var/www/html/.ddev/readme/whichnode.sh)prettier /var/www/html/README.md --cache-location /var/www/html/.ddev/readme/.cache/prettier --write --config=/var/www/html/.ddev/readme/.prettierrc
+$(/var/www/html/.ddev/readme/whichnode.sh)markdown-toc -i /var/www/html/README.md
